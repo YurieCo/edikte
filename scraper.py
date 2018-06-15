323b56e720db1f9f27751b2b52a6a60dd5014712
YurieCo <iurie_cojocari@outlook.com>
2018-06-15 08:55:22 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,7 +21,7 @@ headers = {
 
 session = requests.session()
 br = RoboBrowser(user_agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0)', tries=2, parser='lxml',session = session)
-br.open(base_url)
+# br.open(base_url)
 
 if not os.path.exists('headerImg'):
     os.mkdir('headerImg')
@@ -38,12 +38,14 @@ def info_item(item_url):
     if header_img:
         filename = header_img.split('/')[-1].split('?')[0]
     else:
-        header_img = local_soup.find('div', class_='lazy-res-photo res-photo-thumbnail')['data-original'].split('?')[0]
-        filename = header_img.split('/')[-1].split('?')[0]
+        header_img = local_soup.find('div', class_='lazy-res-photo res-photo-thumbnail')
+        header_img = header_img['data-original'] if header_img else ""
+        filename = header_img.split('/')[-1].split('?')[0] if header_img else ""
 
-    with open('headerImg/{}'.format(filename), 'wb') as f:
-        content = session.get(header_img).content
-        f.write(content)
+    if filename:
+        with open('headerImg/{}'.format(filename), 'wb') as f:
+            content = session.get(header_img).content
+            f.write(content)
 
 
     title = local_soup.select_one('a.ui.large.header.left').text.strip()
@@ -55,67 +57,43 @@ def info_item(item_url):
     cuisine = local_soup.select_one('div.res-info-cuisines.clearfix').text.strip()
 
     votes, phone, avg_costs = local_soup.find_all('span',{'tabindex':0, 'aria-label':True})[:3]
-
+    votes, phone, avg_costs = votes.text.strip(), phone.text.strip(), avg_costs.text.strip()
 
     open_hours = [' '.join(a.strings) for a in local_soup.select_one('#res-week-timetable').find_all('tr')]
     open_hours = ','.join(open_hours)
     address = local_soup.select_one('div.borderless.res-main-address div.resinfo-icon span').text.strip()
-    menue_images = []
 
 
+    with open('zomato-v5b-test.csv','a+') as myfile:
+        wr = csv.writer(myfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    # save into file
-    with open('zomato-v5b-test.csv', 'a') as myfile:
-
-        myfile.write("{}, {}, {}, {}, {}, {}, {}, {},".format(title, loc, loc_type, header_img,
-                                                              filename, rating, votes, phone, cuisine,
-                                                              avg_costs
-                                                              ))
-
-        myfile.write('"{}", {},{}'.format(open_hours, address, item_url))
-
+        l = []
         for img in local_soup.find_all('img',{'class':'lazy-menu-load'}):
             image_url = img['data-original'].split('?')[0]
             filename = image_url.split('/')[-1]
             logging.info('saved with {}'.format(filename))
-            myfile.write(',{}'.format(filename))
+            # myfile.write(',{}'.format(filename))
+            # wr.writerow([filename])
+            l.append(filename)
             content = session.get(image_url).content
             with open('menuImg/{}'.format(filename),'wb') as f:
                 f.write(content)
 
-
-        myfile.write('\n')
-
-
-    # return {
-    #     "Name":title,
-    #     "Location":loc,
-    #     "Type":loc_type,
-    #     "HeaderImg":filename if filename else "",
-    #     "Rating":rating,
-    #     "Reviews":votes,
-    #     "Phone":phone,
-    #     "Cuisine":cuisine,
-    #     "AvgCost":avg_costs,
-    #     "OpeningHours":open_hours,
-    #     "Address":address,
-    #     "MenuImgs":menue_images
-    # }
+        wr.writerow([title, loc, loc_type, header_img, filename, rating, votes, phone, cuisine, avg_costs, open_hours, address,
+             item_url, *l])
 
 
+i = 1
+while i < 9:
+    br.open("https://www.zomato.com/melbourne/dinner-in-ashburton?page={}".format(i))
+    page_content = br.parsed
+    for item in page_content.select(' div.row div.col-s-12 a.result-title.hover_feedback.zred.bold.ln24.fontsize0'):
+        item_url = item.get('href')
+        logging.debug('Now processing {} '.format(item_url))
+        info_item(item_url)
+    i+=1
 
-
-
-
-page_content = br.parsed
-
-
-for item in page_content.select(' div.row div.col-s-12 a.result-title.hover_feedback.zred.bold.ln24.fontsize0'):
-    item_url = item.get('href')
-    logging.debug('Now processing {} '.format(item_url))
-    info_item(item_url)
-
-    print(item)
 
 
 
